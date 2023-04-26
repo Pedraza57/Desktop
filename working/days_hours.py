840e5b2e70bcaec6f4500421a932e9cf5b182194
Miauu <Bryanpedraza57@icloud.com>
2023-04-26 16:40:50 -0700

    control flow

--- working/days_hours.py
+++ working/days_hours.py
@@ -2,16 +2,17 @@ calculation_to_units = 24
 name_of_units = 'hours'
 
 def days_to_units(num_of_day):
-    if num_of_day > 0:
-        return f'{num_of_day} days are {num_of_day * calculation_to_units} {name_of_units}'
-    elif num_of_day == 0:
+    if num_of_day == 0:
         return "You enter a 0, please enter a valid positve number"
     
 def validate_and_execute():
-    if user_input.isdigit():
+    if user_input.isdigit(): 
         user_input_number = int(user_input)
-        calculated_value = days_to_units(user_input_number)
-        print(calculated_value)
+        if user_input_number > 0:
+            calculated_value = days_to_units(user_input_number)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("you have enter 0, please enter a valid positive number")
     else:
         print('your input is not a valid number. Dont ruin my program')
 
