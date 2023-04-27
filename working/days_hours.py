be6b93d90bbaa9c04ad42c69e976ad51bd078063
Miauu <Bryanpedraza57@icloud.com>
2023-04-26 19:20:04 -0700

    working

--- working/days_hours.py
+++ working/days_hours.py
@@ -1,20 +1,23 @@
 calculation_to_units = 24
 name_of_units = 'hours'
 
-def days_to_units(num_of_day):
-    if num_of_day == 0:
-        return "You enter a 0, please enter a valid positve number"
-    
+def days_to_units(num_of_days):
+    return f'{num_of_days} days are {num_of_days*calculation_to_units} {name_of_units}'
+
 def validate_and_execute():
-    if user_input.isdigit(): 
+    try:
         user_input_number = int(user_input)
         if user_input_number > 0:
             calculated_value = days_to_units(user_input_number)
             print(calculated_value)
         elif user_input_number == 0:
-            print("you have enter 0, please enter a valid positive number")
-    else:
-        print('your input is not a valid number. Dont ruin my program')
+            print("you enter a 0, plase enter a valid positive number")
+        else:
+            print("you enter a negative number, no convertion for you")
+    except ValueError:
+        print("your input is not a valid number. dont ruin the program")
+    
 
-user_input = input("Hey user, enter a number of days to convert to hours? \n")
+
+user_input = input('Hey user, enter a number of days and i will convert it to hours! \n')
 validate_and_execute()
