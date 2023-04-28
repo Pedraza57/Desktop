ce19dc0e953cac31213fdc800a4fc33645f84267
Miauu <Bryanpedraza57@icloud.com>
2023-04-27 18:59:16 -0700

    user decision

--- working/days_hours.py
+++ working/days_hours.py
@@ -1,14 +1,17 @@
-calculation_to_units = 24
-name_of_units = 'hours'
 
-def days_to_units(num_of_days):
-    return f'{num_of_days} days are {num_of_days*calculation_to_units} {name_of_units}'
+def days_to_units(num_of_days, convertion_unit):#func days to convert and time_unit
+    if convertion_unit == 'hours':
+        return f'{num_of_days} days are {num_of_days*24} hours'
+    elif convertion_unit == 'minutes':
+        return f"{num_of_days} days are {num_of_days*24*60} minutes"
+    else:
+        return "unsuported unit"
 
 def validate_and_execute():
     try:
-        user_input_number = int(x)
-        if user_input_number > 0:
-            calculated_value = days_to_units(user_input_number)
+        user_input_number = int(days_and_unit_dictionary['days']) #change input to digit from key days
+        if user_input_number > 0: #calling function(first parameter num of days, value of units dic)
+            calculated_value = days_to_units(user_input_number, days_and_unit_dictionary['units'])
             print(calculated_value)
         elif user_input_number == 0:
             print("you enter a 0, plase enter a valid positive number")
@@ -17,10 +20,13 @@ def validate_and_execute():
     except ValueError:
         print("your input is not a valid number. dont ruin the program")
     
-user_input = ''
-while user_input != 'exit': 
-    user_input = input('Hey user, enter number of days as a comma seperated list and i will convert it to hours!\n')
-    for x in set(user_input.split(',')):
-        validate_and_execute() 
+user_input = '' # to start the while True
+while user_input != 'exit':  #to end the program
+    user_input = input('Hey user, enter number of days and convertion units\n') #input by user 
+    days_and_unit = user_input.split(':') #input answer seperated by ':'
+    days_and_unit_dictionary = {"days": days_and_unit[0], "units": days_and_unit[1]} #making the dictionary and which values to pull from 
+    print(days_and_unit) #showing what we have 
+    print(days_and_unit_dictionary) #showing the dictionarys set
+    validate_and_execute() #executing the second function 
 
 
